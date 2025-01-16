10424350178de9273eeae237ccc713c399e0ebb5
ashikeelahi3 <ashikeelahi3@gmail.com>
2025-01-17 02:24:41 +0600

    Product information

---
+++ data/data.ts
@@ -0,0 +1,81 @@
+
+
+
+export const products = [
+  {
+    id: 1,
+    name: 'Rice',
+    unit: 'kg',
+    purchaseOption: ["Loose", "Packet", "Other"],
+    categories: ["Miniket", "Nazirshall", "Paijam", "Sharna", "Lota", "Other"],
+  },
+  {
+    id: 2,
+    name: 'Flour',
+    unit: 'kg',
+    purchaseOption: ["Loose", "Packet", "Other"],
+  },
+  {
+    id: 3,
+    name: 'Lentil',
+    unit: 'kg',
+    purchaseOption: ["Loose", "Packet", "Other"],
+  },
+  {
+    id: 4,
+    name: "Soybean Oil",
+    unit: 'liter',
+    purchaseOption: ["Loose", "Bottle", "Other"],
+  },
+  {
+    id: 5,
+    name: "Salt",
+    unit: 'kg',
+    purchaseOption: ["Loose", "Packet", "Other"],
+  },
+  {
+    id: 6,
+    name: "Sugar",
+    unit: 'kg',
+    purchaseOption: ["Loose", "Packet", "Other"],
+    categories: ["Deshi", "Indian", "Pakisthani", "Other"]
+  },
+  {
+    id: 7,
+    name: "Egg",
+    unit: '4 pieces'
+  },
+  {
+    id: 8,
+    name: "Chicken",
+    unit: 'kg',
+    purchaseOption: ["Live", "Processed with skin","Processed without skin", "Other"],
+    categories: ["Broiler", "Layer", "Sonali", "Other"]
+  },
+  {
+    id: 9,
+    name: "Potato",
+    unit: 'kg',
+    categories: ["New Potato", "Old Potato", "Other"],
+  },
+  {
+    id: 10,
+    name: "Eggplant",
+    unit: 'kg',
+  },
+  {
+    id: 11,
+    name: "Onion",
+    unit: 'kg',
+    categories: ["Deshi", "Indian", "Pakisthani", "Other"],
+  }, 
+  {
+    id: 12,
+    name: "Green Chilli",
+    unit: 'kg',
+  }
+]
+
+export const onlineShops = [
+  "Agora", "Ajkerdeal", "Amana big bazar", "Bangla shoppers", "Chaldal", "Daily Bazar", "Daily Shopping", "Daraz mart", "GOFRESH", "Grocery", "Jogaan", "Khaas Food", "Kintaini", "Meena bazar", "Meenaclick", "MudiiMart", "Othoba", "Paikaree.com.bd", "SobjiBazaar", "Shwapno", "Taja Bajar", "Unimart", "Uttara Kacha Bazaar"
+]
